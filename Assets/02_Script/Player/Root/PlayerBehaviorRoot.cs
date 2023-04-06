18567859817ad7fa356a4d2d4129ab11c2a06a70
dkdkdsa <bos94330@gmail.com>
2023-04-06 18:52:51 +0900

    PlayerBehaviorRoot

---
+++ Assets/02_Script/Player/Root/PlayerBehaviorRoot.cs
@@ -0,0 +1,12 @@
+using Interface;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerBehaviorRoot : PlayerRoot, IEventObject
+{
+    public abstract void AddEvent();
+
+    public abstract void RemoveEvent();
+
+}
