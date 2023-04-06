de28c9b87f340d681641731f5c25c96bba84693d
dkdkdsa <bos94330@gmail.com>
2023-04-06 18:47:37 +0900

    PlayerRoot

---
+++ Assets/02_Script/Player/Root/PlayerRoot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoot : MonoBehaviour
+{
+
+    protected Animator animator;
+    protected SpriteRenderer spriteRenderer;
+    protected Rigidbody2D rigid;
+
+    protected virtual void Awake()
+    {
+
+
+
+    }
+
+}
