9a142be5437149debab8c1ae5a0bf764cea7591a
dkdkdsa <bos94330@gmail.com>
2023-04-06 19:47:33 +0900

    PlayerMove

--- Assets/02_Script/Player/Movement/PlayerMove.cs
+++ Assets/02_Script/Player/Movement/PlayerMove.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class PlayerMove : PlayerBehaviorRoot
 {
-    
 
+    [SerializeField] private float moveSpeed;
+
+    public override void AddEvent()
+    {
+
+        actionSystem.OnHorizontalEvnet += Move;
+
+    }
+
+    public override void RemoveEvent()
+    {
+
+        actionSystem.OnHorizontalEvnet -= Move;
+
+    }
+
+    private void Move(float inputX)
+    {
+
+        rigid.velocity = new Vector2(inputX * moveSpeed, rigid.velocity.y);
+
+    }
 
 }
