e222329397221c575cca15e4cb1de75386473b89
dkdkdsa <bos94330@gmail.com>
2023-04-12 19:21:34 +0900

    Trap

--- Assets/02_Script/Object/TrapObject/Trap.cs
+++ Assets/02_Script/Object/TrapObject/Trap.cs
@@ -1,18 +1,16 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
+        //DamageCodeHere;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
