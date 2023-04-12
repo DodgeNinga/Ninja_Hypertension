513b1fa18b3bc4d09c445b0869f8aad78c26386a
dkdkdsa <bos94330@gmail.com>
2023-04-12 20:21:23 +0900

    PlayerActionSystem

--- Assets/02_Script/Player/Input/PlayerActionSystem.cs
+++ Assets/02_Script/Player/Input/PlayerActionSystem.cs
@@ -9,9 +9,12 @@ public class PlayerActionSystem : MonoBehaviour
     public event Action<float> OnHorizontalEvnet;
     public event Action<float> OnVerticalEvnet;
     public event Action OnJumpKeyEvent;
+    public event Action OnSkillKeyPressEvent;
+    //여기에 키 인풋코드 추가
 
     public void OnHorizontalExecute(float v) => OnHorizontalEvnet?.Invoke(v);
     public void OnVerticalExecute(float v) => OnVerticalEvnet?.Invoke(v);
     public void OnJumpKeyExecute() => OnJumpKeyEvent?.Invoke();
+    public void OnSkillKeyPress() => OnSkillKeyPressEvent?.Invoke();
 
 }
