973c511a043289dd28c0639af6eee983ef1b52d8
dkdkdsa <bos94330@gmail.com>
2023-04-19 19:14:30 +0900

    Animator

---
+++ Assets/02_Script/Player/Controll/PlayerAnimator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+
+    private readonly int MoveVelXHash = Animator.StringToHash("MoveVelX");
+    private readonly int MoveVelYHash = Animator.StringToHash("MoveVelY");
+    private readonly int JunpHash = Animator.StringToHash("Jump");
+    private readonly int LandingHash = Animator.StringToHash("Landing");
+    private readonly int LandingTriggerHash = Animator.StringToHash("LandingTrigger");
+
+    private Animator animator;
+    private Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+
+        SetMoveVelHash();
+
+    }
+
+    private void SetMoveVelHash()
+    {
+
+        animator.SetFloat(MoveVelXHash, Mathf.Abs(rigid.velocity.x));
+        animator.SetFloat(MoveVelYHash, rigid.velocity.y);
+
+    }
+
+}
