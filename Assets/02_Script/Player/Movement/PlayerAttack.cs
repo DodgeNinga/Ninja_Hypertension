1fcdc6127a60ae3edca4d051fe9976e8fbc6258c
dkdkdsa <bos94330@gmail.com>
2023-04-20 10:00:39 +0900

    PlayerAttack

---
+++ Assets/02_Script/Player/Movement/PlayerAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : PlayerBehaviorRoot
+{
+
+    [SerializeField] private float comboAbleTime = 0.2f;
+
+    public override void AddEvent()
+    {
+        
+    }
+
+    public override void RemoveEvent()
+    {
+        
+    }
+}
