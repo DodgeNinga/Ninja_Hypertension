cc7dd8cc85f9ae6deb2559a86fcfffa776312602
dkdkdsa <bos94330@gmail.com>
2023-04-20 10:03:17 +0900

    PlayerActionSystem

--- Assets/02_Script/Player/Input/PlayerActionSystem.cs
+++ Assets/02_Script/Player/Input/PlayerActionSystem.cs
@@ -11,12 +11,14 @@ public class PlayerActionSystem : MonoBehaviour
     public event Action<float> OnHorizontalRawEvent;
     public event Action OnJumpKeyEvent;
     public event Action OnSkillKeyPressEvent;
+    public event Action OnSkillKeyUpEvent;
     //여기에 키 인풋코드 추가
 
     public void OnHorizontalExecute(float v) => OnHorizontalEvent?.Invoke(v);
     public void OnVerticalExecute(float v) => OnVerticalEvnet?.Invoke(v);
     public void OnHorizontalRawExecute(float v) => OnHorizontalRawEvent?.Invoke(v);
     public void OnJumpKeyExecute() => OnJumpKeyEvent?.Invoke();
-    public void OnSkillKeyPress() => OnSkillKeyPressEvent?.Invoke();
+    public void OnSkillKeyPressExecute() => OnSkillKeyPressEvent?.Invoke();
+    public void OnSkillKeyUpExecute() => OnSkillKeyUpEvent?.Invoke();
 
 }
--- Assets/02_Script/Player/Movement/PlayerAttack.cs
+++ Assets/02_Script/Player/Movement/PlayerAttack.cs
@@ -7,13 +7,28 @@ public class PlayerAttack : PlayerBehaviorRoot
 
     [SerializeField] private float comboAbleTime = 0.2f;
 
+    private int comboCnt;
+
+    protected override void Awake()
+    {
+
+        base.Awake();
+
+        AddEvent();
+
+    }
+
     public override void AddEvent()
     {
         
+
+
     }
 
     public override void RemoveEvent()
     {
         
+
+
     }
 }
