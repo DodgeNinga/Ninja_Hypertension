4acb8fe7b4f5cf3c235bbb99bec385893452a7f9
dkdkdsa <bos94330@gmail.com>
2023-04-23 20:37:42 +0900

    PlayerDash

---
+++ Assets/02_Script/Player/Movement/PlayerDash.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : PlayerBehaviorRoot
+{
+    public override void AddEvent()
+    {
+        
+    }
+
+    public override void RemoveEvent()
+    {
+        
+    }
+}
