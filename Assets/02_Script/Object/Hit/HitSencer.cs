a46c42b015a6e703bf7c63a19b323bac3b3a08b5
dkdkdsa <bos94330@gmail.com>
2023-04-24 20:03:13 +0900

    HitSencer

--- Assets/02_Script/Object/Hit/HitSencer.cs
+++ Assets/02_Script/Object/Hit/HitSencer.cs
@@ -16,11 +16,11 @@ public class HitSencer : MonoBehaviour
 
         var hit = hitRanges.Find(x => x.atkKey == hitName);
 
-        var hitArr = Physics2D.BoxCastAll(
+        var hitArr = Physics2D.OverlapBoxAll(
         swap ? 
         transform.position + new Vector3(-hit.offSet.x, hit.offSet.y) : 
-        transform.position - new Vector3(hit.offSet.x, hit.offSet.y),
-        hit.range, 0, Vector2.zero, 0, hitAbleLayer);
+        transform.position + new Vector3(hit.offSet.x, hit.offSet.y),
+        hit.range, 0,hitAbleLayer);
         if(hitArr.Length != 0)
         {
             foreach(var item in hitArr)
@@ -37,7 +37,7 @@ public class HitSencer : MonoBehaviour
         foreach(var item in hitRanges)         
         {
             Gizmos.color = item.gizmoColor;
-            Gizmos.DrawWireCube(swap ? transform.position + new Vector3(-item.offSet.x, item.offSet.y) : transform.position - new Vector3(item.offSet.x, item.offSet.y), item.range);;
+            Gizmos.DrawWireCube(swap ? transform.position + new Vector3(-item.offSet.x, item.offSet.y) : transform.position + new Vector3(item.offSet.x, item.offSet.y), item.range);;
             Gizmos.color = Color.white;
 
         }
