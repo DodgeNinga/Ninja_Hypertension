cc4814df27445fbff92dbc130dcaac6db59ce814
gongsy1221 <gongsy0724@naver.com>
2023-04-25 21:58:48 +0900

    EnemyAttack

--- Assets/02_Script/Enemy/EnemyAttack.cs
+++ Assets/02_Script/Enemy/EnemyAttack.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float power;
+    public float delayTime = 0f;
+    bool playerAttack;
+
+    bool _isPlayerAttacked;
+    bool _isPlayerAttackedMethodRunning;
+
     void Start()
+    {
+        _isPlayerAttacked = false;
+        _isPlayerAttackedMethodRunning = false;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void onTriggerEnterAndStayMethod(Collider2D collision)
     {
-        
+        if (collision.gameObject.name == "Player" && !_isPlayerAttacked && !_isPlayerAttackedMethodRunning)
+        {
+            _isPlayerAttacked = true;
+            _isPlayerAttackedMethodRunning = true;
+            StartCoroutine("PlayerAttack", delayTime);
+            Debug.Log("attack");
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        onTriggerEnterAndStayMethod(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        onTriggerEnterAndStayMethod(collision);
+    }
+
+
+
+
+    IEnumerator PlayerAttack(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        _isPlayerAttacked = false;
+        _isPlayerAttackedMethodRunning = false;
+
+        Debug.Log("isNotAttacked");
     }
 }
