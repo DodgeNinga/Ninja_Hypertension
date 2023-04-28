64b017873aff8442c4e042272f34c362689fd7f2
dkdkdsa <bos94330@gmail.com>
2023-04-28 12:45:52 +0900

    TextStory

--- Assets/02_Script/System/Story/TextStory.cs
+++ Assets/02_Script/System/Story/TextStory.cs
@@ -1,5 +1,6 @@
 using Class;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,7 +9,7 @@ using UnityEngine.Events;
 public class TextStory : MonoBehaviour
 {
 
-    
+    [SerializeField] private List<StoryTextBaseClass> stories;
 
     private IEnumerator SetTextCo(TMP_Text text, string endValue, 
         float showTextTime, UnityEvent apertureEvent, UnityEvent endEvent)
