349ac8aec96aa6fc0dbc3023182d56a259498e95
dkdkdsa <bos94330@gmail.com>
2023-05-03 00:47:25 +0900

    PlayerDamageCaster

--- Assets/02_Script/Player/Event/PlayerDamageCaster.cs
+++ Assets/02_Script/Player/Event/PlayerDamageCaster.cs
@@ -26,14 +26,12 @@ public class PlayerDamageCaster : MonoBehaviour
         if (itemObj == null) return;
 
         float damageValue = itemObj.GetDamageValue(playerHP.GetHPLV);
-
         hpObj.TakeDamage(damageValue);
 
         var obj = FAED.Pop("DamageText", hpObj.transform.position +
             new Vector3(Random.Range(-1, 1f), Random.Range(-1, 1f)), Quaternion.identity);
 
         var damage = obj.GetComponent<DamageText>();
-
         damage.SetText(damageValue);
 
     }
