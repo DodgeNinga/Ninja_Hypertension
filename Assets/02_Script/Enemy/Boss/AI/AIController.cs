0208c431ff8fa03017850e8c9a4eed0279cc2c51
dkdkdsa <bos94330@gmail.com>
2023-05-06 18:59:26 +0900

    AIController

--- Assets/02_Script/Enemy/Boss/AI/AIController.cs
+++ Assets/02_Script/Enemy/Boss/AI/AIController.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class AIController : MonoBehaviour
 {
 
+    private AIState currentState;
+
     public Rigidbody2D enemyRigid { get; private set; }
 
     private void Awake()
     {
         
         enemyRigid = GetComponent<Rigidbody2D>();
+
+    }
+
+    public void ChangeState(AIState state)
+    {
+
+        currentState.ExitState();
+        currentState = state;
 
     }
 
