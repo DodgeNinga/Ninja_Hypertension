e3bfe5a0cbddce9759f22fc5359c7fa7b3cf00b1
dkdkdsa <bos94330@gmail.com>
2023-05-06 18:51:01 +0900

    SettingTransition

--- Assets/02_Script/Enemy/Boss/AI/AIState.cs
+++ Assets/02_Script/Enemy/Boss/AI/AIState.cs
@@ -6,6 +6,16 @@ public abstract class AIState : MonoBehaviour
 {
 
     private List<AITransition> transitions = new List<AITransition>();
+    private AIController controller;
+
+    protected virtual void Awake()
+    {
+
+        GetComponentsInChildren(transitions);
+        controller = transform.parent.parent.GetComponent<AIController>();
+
+    }
+
     public abstract void EnterState(); 
     public abstract void UpdateState();
     public abstract void ExitState();
