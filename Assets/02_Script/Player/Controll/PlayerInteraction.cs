064a585071ab59653a8387600e541d9cbb1a5d56
dkdkdsa <bos94330@gmail.com>
2023-05-07 03:03:42 +0900

    PlayerInteraction

--- Assets/02_Script/Player/Controll/PlayerInteraction.cs
+++ Assets/02_Script/Player/Controll/PlayerInteraction.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    
 
+    [SerializeField] private LayerMask interactionLayer;
+    [SerializeField] private TMP_Text interactionText;
+    [SerializeField] private Vector2 interactionRange, interactionOffset;
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        
+
+
+    }
+
+#endif
 
 }
