7f449b7b0e7f1e9902fe0ec15e3512992ad5a712
dkdkdsa <bos94330@gmail.com>
2023-05-07 03:06:35 +0900

    Gizmo

--- Assets/02_Script/Player/Controll/PlayerInteraction.cs
+++ Assets/02_Script/Player/Controll/PlayerInteraction.cs
@@ -9,13 +9,17 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] private LayerMask interactionLayer;
     [SerializeField] private TMP_Text interactionText;
     [SerializeField] private Vector2 interactionRange, interactionOffset;
+    [SerializeField] private Color gizmoColor = Color.black;
 
 #if UNITY_EDITOR
 
     private void OnDrawGizmos()
     {
         
-
+        var old = Gizmos.color;
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireCube(transform.position + (Vector3)interactionOffset, interactionRange);
+        Gizmos.color = old;
 
     }
 
