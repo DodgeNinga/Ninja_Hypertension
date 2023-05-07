76efbda5106f4e546690955fe93e108cab91f68a
dkdkdsa <bos94330@gmail.com>
2023-05-08 00:12:22 +0900

    GolemAnimator

--- Assets/02_Script/Enemy/Boss/Golem/GolemAnimator.cs
+++ Assets/02_Script/Enemy/Boss/Golem/GolemAnimator.cs
@@ -7,6 +7,9 @@ public class GolemAnimator : MonoBehaviour
 
     private readonly int IsMoveHash = Animator.StringToHash("IsMove");
     private readonly int WakeUpHash = Animator.StringToHash("WakeUp");
+    private readonly int IsAttackHash = Animator.StringToHash("IsAttack");
+    private readonly int AttackTriggerHash = Animator.StringToHash("AttackTrigger");
+    private readonly int AttackSelectIntHash = Animator.StringToHash("AttackSelectInt");
 
     private Animator animator;
 
@@ -18,6 +21,9 @@ public class GolemAnimator : MonoBehaviour
     }
 
     public void SetIsMove(bool value) => animator.SetBool(IsMoveHash, value);
+    public void SetIsAttack(bool value) => animator.SetBool(IsAttackHash, value);
+    public void SetAttackSelectInt(int value) => animator.SetInteger(AttackSelectIntHash, value);
     public void SetWakeUp() => animator.SetTrigger(WakeUpHash);
+    public void SetAttackTrigger() => animator.SetTrigger(AttackTriggerHash);
 
 }
