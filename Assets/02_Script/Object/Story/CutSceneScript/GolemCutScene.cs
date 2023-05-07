7861b063625e955a9ccef790bd6f07754a6c837d
dkdkdsa <bos94330@gmail.com>
2023-05-07 21:15:28 +0900

    GolemCutScene

--- Assets/02_Script/Object/Story/CutSceneScript/GolemCutScene.cs
+++ Assets/02_Script/Object/Story/CutSceneScript/GolemCutScene.cs
@@ -1,20 +1,45 @@
+using FD.Dev;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GolemCutScene : CutScene
 {
+
+
+    private CameraTakeManager camTakeManager;
+
+    private void Awake()
+    {
+        
+        camTakeManager = FindObjectOfType<CameraTakeManager>();
+
+    }
+
     public override void StartCutScene()
     {
 
+        camTakeManager.StartTake("Golem");
+        FAED.InvokeDelayReal(() =>
+        {
 
+            CameraManager.instance.Shake(2f, 2f, 1f, true);
+
+            FAED.InvokeDelayReal(() =>
+            {
+
+                thisCutScene.EndCutScene();
+
+            }, 1f);
+
+        }, 1f);
 
     }
 
     public override void EndCutScene()
     {
 
-
+        //
 
     }
 
