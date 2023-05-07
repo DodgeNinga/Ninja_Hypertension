2e92dc2e2aad7b23d689a0d34de7d6c8abb30626
dkdkdsa <bos94330@gmail.com>
2023-05-08 00:33:52 +0900

    AIAnimeEventController

--- Assets/02_Script/Enemy/Boss/AI/AIAnimeEventController.cs
+++ Assets/02_Script/Enemy/Boss/AI/AIAnimeEventController.cs
@@ -1,10 +1,20 @@
+using Class;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AIAnimeEventController : MonoBehaviour
 {
-    
 
+    [SerializeField] private List<AIAnimeEventClass> events = new List<AIAnimeEventClass>();
+    
+    public void SetEvent(string value)
+    {
+
+        var cls = events.Find(x => x.key == value);
+
+        cls.evt?.Invoke();
+
+    }
 
 }
