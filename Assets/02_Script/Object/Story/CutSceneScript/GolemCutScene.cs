3258c9738c73c07100ae22e47de35cc1281435aa
dkdkdsa <bos94330@gmail.com>
2023-05-07 20:51:37 +0900

    GolemCutScene

---
+++ Assets/02_Script/Object/Story/CutSceneScript/GolemCutScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemCutScene : CutScene
+{
+    public override void StartCutScene()
+    {
+
+
+
+    }
+
+    public override void EndCutScene()
+    {
+
+
+
+    }
+
+}
