7d338869491dc2f8447fbcec2bed600daa8b47ac
dkdkdsa <bos94330@gmail.com>
2023-05-07 23:54:10 +0900

    GolemAttackState

---
+++ Assets/02_Script/Enemy/Boss/Golem/State/GolemAttackState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class GolemAttackState : AIState
+{
+
+    [SerializeField] private float coolTimeMin, coolTimeMax;
+
+    public override void EnterState()
+    {
+
+        StartCoroutine(SkillSelectCo());       
+
+    }
+
+    public override void ExitState()
+    {
+    }
+
+    public override void UpdateState()
+    {
+    }
+
+    private IEnumerator SkillSelectCo()
+    {
+
+        yield return new WaitForSecondsRealtime(Random.Range(coolTimeMin, coolTimeMax));
+
+    }
+
+}
