fd1af1fdebb2e09e80a5d7d8682dd5aee6c52135
dkdkdsa <bos94330@gmail.com>
2023-05-08 18:27:26 +0900

    MeleeAttackRangeTransition

--- Assets/02_Script/Enemy/Boss/Golem/Transition/MeleeAttackRangeTransition.cs
+++ Assets/02_Script/Enemy/Boss/Golem/Transition/MeleeAttackRangeTransition.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 public class MeleeAttackRangeTransition : AITransition
 {
 
+    [SerializeField] private Transform target;
+    [SerializeField] private Color gizmoColor = Color.black;
+    [SerializeField] private float range;
+    [SerializeField] private bool reverce;
+
     private GolemMeleeAttackState melState;
 
     private void Awake()
     {
         
-
+        melState = FindObjectOfType<GolemMeleeAttackState>();
 
     }
 
     public override bool MakeTransition()
     {
 
-        return false;
+        bool value = reverce ? Vector2.Distance(target.position, transform.position) >= range
+    : Vector2.Distance(target.position, transform.position) < range;
+
+        return value && !melState.isCoolDown;
 
     }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+
+        var old = Gizmos.color;
+        Gizmos.color = gizmoColor;
+
+        Gizmos.DrawWireSphere(transform.position, range);
+
+        Gizmos.color = old;
+
+    }
+
+#endif
 
 }
