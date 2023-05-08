21db6b0ba7527d1d7dfac73d37dd77da9551cc0d
dkdkdsa <bos94330@gmail.com>
2023-05-08 22:38:06 +0900

    BirdObject

--- Assets/02_Script/Object/MapObj/BirdObject.cs
+++ Assets/02_Script/Object/MapObj/BirdObject.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class BirdObject : MonoBehaviour
 {
 
+    private readonly int MoveTriggerHash = Animator.StringToHash("MoveTrigger");
+
     [SerializeField] private Vector2 moveDir;
 
+    private Animator animator;
     private Rigidbody2D rigid;
 
     private void Awake()
     {
         
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
     }
 
-    public void SetStart()
+    public void MoveStart()
     {
 
         rigid.velocity = moveDir;
+        animator.SetTrigger(MoveTriggerHash);
 
     }
 
