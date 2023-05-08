1d6a6832bf11c7fdbe416cea14f7a34eea30c1f9
dkdkdsa <bos94330@gmail.com>
2023-05-09 01:46:27 +0900

    CustomSpriteParticle

--- Assets/02_Script/Object/Effect/CustomSpriteParticle.cs
+++ Assets/02_Script/Object/Effect/CustomSpriteParticle.cs
@@ -16,4 +16,20 @@ public class CustomSpriteParticle : MonoBehaviour
 
     }
 
+    public void PlayParticle()
+    {
+
+        var texture = mainSprite.texture;
+
+        var makeSprite = Sprite.Create(texture,
+            new Rect(0,
+            0, texture.width / Random.Range(2, 4), texture.height / Random.Range(2, 4)),
+            Vector2.one / 2, mainSprite.pixelsPerUnit);
+
+        particle.textureSheetAnimation.AddSprite(makeSprite);
+
+        particle.Play();
+
+    }
+
 }
