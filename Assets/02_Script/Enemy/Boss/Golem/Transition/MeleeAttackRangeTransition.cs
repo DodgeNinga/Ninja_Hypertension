fd3d14d62c5f0dac8bb54ae72f60f60df02659c4
dkdkdsa <bos94330@gmail.com>
2023-05-08 18:22:27 +0900

    MeleeAttackRangeTransition

---
+++ Assets/02_Script/Enemy/Boss/Golem/Transition/MeleeAttackRangeTransition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackRangeTransition : AITransition
+{
+
+
+
+    public override bool MakeTransition()
+    {
+
+        return false;
+
+    }
+
+}
