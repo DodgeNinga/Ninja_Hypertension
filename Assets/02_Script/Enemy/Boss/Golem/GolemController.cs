70b9beab2d8ec3ea79afef288ea723f64e2a7e75
dkdkdsa <bos94330@gmail.com>
2023-05-08 17:33:05 +0900

    GolemController

--- Assets/02_Script/Enemy/Boss/Golem/GolemController.cs
+++ Assets/02_Script/Enemy/Boss/Golem/GolemController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GolemContriller : AIController
+public class GolemController : AIController
 {
 
     private HitSencer sencer;
