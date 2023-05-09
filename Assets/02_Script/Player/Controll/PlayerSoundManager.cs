ac5193d73fc921fe4e22add510667d7c8df09181
dkdkdsa <bos94330@gmail.com>
2023-05-10 00:11:10 +0900

    PlayerSoundManager

--- Assets/02_Script/Player/Controll/PlayerSoundManager.cs
+++ Assets/02_Script/Player/Controll/PlayerSoundManager.cs
@@ -1,3 +1,4 @@
+using Class;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,20 @@ using UnityEngine;
 public class PlayerSoundManager : MonoBehaviour
 {
 
-    
+    [SerializeField] private List<SoundEventClass> soundEvents = new List<SoundEventClass>();
+
+    public void PlaySound(string key)
+    {
+
+        var obj = soundEvents.Find(x => x.key == key);
+
+        if(obj != null) 
+        { 
+            
+            obj.soundSource.Play();
+        
+        }
+
+    }
 
 }
