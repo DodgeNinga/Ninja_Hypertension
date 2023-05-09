d18fbc89d5ca87b9924c4dc314d8984ad4e0beb2
dkdkdsa <bos94330@gmail.com>
2023-05-10 00:22:49 +0900

    PlayerSoundManager

--- Assets/02_Script/Player/Controll/PlayerSoundManager.cs
+++ Assets/02_Script/Player/Controll/PlayerSoundManager.cs
@@ -1,12 +1,63 @@
 using Class;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerSoundManager : MonoBehaviour
+public class PlayerSoundManager : PlayerRoot
 {
 
     [SerializeField] private List<SoundEventClass> soundEvents = new List<SoundEventClass>();
+
+    private float fallingTime;
+
+    private void Update()
+    {
+
+        FallingChack();
+        PlayLandingSound();
+
+    }
+
+    private void FallingChack()
+    {
+
+        if (!jumpCol.isGround)
+        {
+
+            fallingTime += Time.deltaTime;
+
+        }
+
+    }
+
+    private void PlayLandingSound()
+    {
+
+        if(fallingTime > 1 && jumpCol.isGround) 
+        {
+
+            PlaySound("Landing");
+        
+        }
+
+    }
+
+    public void PlayAttackSound()
+    {
+
+        PlaySound("Attack");
+
+    }
+
+    public void PlayJumpSound()
+    {
+
+        if (jumpCol.isGround) return;
+        
+        PlaySound("JumpSound");
+
+    }
 
     public void PlaySound(string key)
     {
