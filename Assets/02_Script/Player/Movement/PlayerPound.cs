a454fece95c156a2342194441a9225480510ece7
dkdkdsa <bos94330@gmail.com>
2023-06-07 00:14:01 +0900

    PlayerPound

---
+++ Assets/02_Script/Player/Movement/PlayerPound.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPound : PlayerBehaviorRoot
+{
+
+    protected override void Awake()
+    {
+
+        base.Awake();
+
+        AddEvent();
+
+    }
+
+    private void Pounding()
+    {
+
+
+
+    }
+
+    public override void AddEvent()
+    {
+
+
+
+    }
+
+    public override void RemoveEvent()
+    {
+
+
+
+    }
+}
