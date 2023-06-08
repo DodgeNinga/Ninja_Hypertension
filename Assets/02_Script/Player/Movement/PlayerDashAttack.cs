df0d5de375054b84df8a8233599ba342acca00f3
dkdkdsa <bos94330@gmail.com>
2023-06-08 19:47:09 +0900

    PlayerDashAttack

--- Assets/02_Script/Player/Movement/PlayerDashAttack.cs
+++ Assets/02_Script/Player/Movement/PlayerDashAttack.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,16 @@ public class PlayerDashAttack : PlayerBehaviorRoot
 {
 
     [SerializeField] private PlayerBehaviorRoot[] removeEvts;
+    [SerializeField] private float dashPower;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float skillCoolTime;
 
     private PlayerMove playerMove;
+    private ParticlePlayer particlePlayer;
+    private PlayerInvincibility invincibility;
+    private PlayerSoundManager soundManager;
     private bool isDashHolding = false;
+    private bool isSkillCoolDown = false;
 
     protected override void Awake()
     {
@@ -18,6 +26,8 @@ public class PlayerDashAttack : PlayerBehaviorRoot
         AddEvent();
 
         playerMove = GetComponent<PlayerMove>();
+        particlePlayer = transform.Find("ParticlePlayer").Find("DashParticle").GetComponent<ParticlePlayer>();
+        soundManager = GetComponent<PlayerSoundManager>();
 
     }
 
@@ -26,7 +36,8 @@ public class PlayerDashAttack : PlayerBehaviorRoot
 
         if (!jumpCol.isGround ||
             playerControllValue.isHoldAttack ||
-            playerControllValue.isAnySkillAttack) return;
+            playerControllValue.isAnySkillAttack ||
+            isSkillCoolDown) return;
 
         playerControllValue.isAnySkillAttack = true;
         isDashHolding = true;
@@ -43,6 +54,16 @@ public class PlayerDashAttack : PlayerBehaviorRoot
 
         isDashHolding = false;
         animator.SetDashAttackHoldingEndTrigger();
+        
+        StartCoroutine(StartDashCo());
+        StartCoroutine(DashEndCo());
+
+
+    }
+
+    private void DashEnd()
+    {
+
         playerMove.SetMoveSpeed(-1);
 
     }
@@ -60,6 +81,36 @@ public class PlayerDashAttack : PlayerBehaviorRoot
 
         actionSystem.OnDashAttackKeyPressEvent -= DashAttack;
         actionSystem.OnHoldSkillKeyUpEvent -= EndHold;
+
+    }
+
+    private IEnumerator StartDashCo()
+    {
+
+        yield return null;
+
+        soundManager.PlayDashSound();
+        float curDashPower = spriteRenderer.flipX ? -dashPower : dashPower;
+        particlePlayer.StartPlay(transform.position, Quaternion.identity, spriteRenderer.flipX switch
+        {
+
+            true => -1,
+            false => 1
+
+        }, transform);
+
+        rigid.velocity = new Vector2(curDashPower, 0);
+
+    }
+
+    private IEnumerator DashEndCo()
+    {
+
+        yield return new WaitForSeconds(dashTime);
+
+        DashEnd();
+
+        yield return new WaitForSeconds(skillCoolTime);
 
     }
 
