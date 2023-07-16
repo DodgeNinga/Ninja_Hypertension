f05ef9085ca4c55308b246199081301d97737c17
dkdkdsa <bos94330@gmail.com>
2023-07-17 00:45:24 +0900

    ReaperHP

---
+++ Assets/02_Script/Enemy/Boss/Reaper/ReaperHP.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaperHP : HPObject
+{
+    
+
+
+
+
+    private void Update()
+    {
+        
+        DieChack();
+
+    }
+
+    private void DieChack()
+    {
+
+
+
+    }
+
+}
