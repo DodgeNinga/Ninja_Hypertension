9b484b8fdf83d181912f9cf6d5a91eb64b97e004
dkdkdsa <bos94330@gmail.com>
2023-07-16 23:19:19 +0900

    ReaperMoveState

--- Assets/02_Script/Enemy/Boss/Reaper/ReaperController.cs
+++ Assets/02_Script/Enemy/Boss/Reaper/ReaperController.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReaperController : MonoBehaviour
+public class ReaperController : AIController
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private HitSencer hitSencer;
+    private SpriteRenderer spriteRenderer;
+
+    protected override void Awake()
     {
-        
+
+        base.Awake();
+
+        hitSencer = GetComponent<HitSencer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+
+        base.Update();
+
+        hitSencer.swap = spriteRenderer.flipX;
+
     }
+
 }
---
+++ Assets/02_Script/Enemy/Boss/Reaper/State/ReaperMoveState.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaperMoveState : AIState
+{
+
+    [SerializeField] private float moveSpeed = 3f;
+
+    private Transform target;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigid;
+
+    protected override void Awake()
+    {
+
+        base.Awake();
+
+        target = GameObject.Find("Player").transform;
+        spriteRenderer = controller.GetComponent<SpriteRenderer>();
+
+    }
+
+    public override void EnterState()
+    {
+
+
+
+    }
+
+    public override void ExitState()
+    {
+
+        rigid.velocity = Vector3.zero;
+
+    }
+
+    public override void UpdateState()
+    {
+
+        float crtSpeed = (transform.position.x - target.position.x) > 0 ? -moveSpeed : moveSpeed;
+
+        rigid.velocity = new Vector2(crtSpeed, 0);
+        spriteRenderer.flipX = (transform.position.x - target.position.x) > 0;
+
+    }
+
+}
