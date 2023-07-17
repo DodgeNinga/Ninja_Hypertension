cb1dbf3032cb6884bf34967a019164587820fd0c
dkdkdsa <bos94330@gmail.com>
2023-07-17 23:21:54 +0900

    Boss

--- Assets/02_Script/Object/Skill/LineSkill.cs
+++ Assets/02_Script/Object/Skill/LineSkill.cs
@@ -1,18 +1,41 @@
+using FD.Dev;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LineSkill : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private LineEffect lineEffect;
+    [SerializeField] private GameObject waringLine;
+
+    public void SetSkill()
     {
-        
+
+        lineEffect.gameObject.SetActive(true);
+        waringLine.SetActive(false);
+        lineEffect.Show();
+
+        var obj = Physics2D.OverlapBox(transform.position, new Vector2(10, 0.4f), transform.eulerAngles.z, LayerMask.GetMask("Player"));
+
+        if(obj != null)
+        {
+
+            obj.GetComponent<HPObject>()?.TakeDamage(10);
+            obj.GetComponent<FeedBackManager>()?.PlayFeedback("Hit");
+
+        }
+
+        FAED.InvokeDelay(() =>
+        {
+
+            lineEffect.gameObject.SetActive(false);
+            waringLine.gameObject.SetActive(true);
+
+            FAED.Push(gameObject);
+
+        }, 0.5f);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
