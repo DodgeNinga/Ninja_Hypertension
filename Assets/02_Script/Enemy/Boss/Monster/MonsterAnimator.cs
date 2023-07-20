76ea8def3b04a0582b3d3778243131a5c0177702
dkdkdsa <bos94330@gmail.com>
2023-07-21 00:07:57 +0900

    MonsterAnimator

--- Assets/02_Script/Enemy/Boss/Monster/MonsterAnimator.cs
+++ Assets/02_Script/Enemy/Boss/Monster/MonsterAnimator.cs
@@ -6,7 +6,9 @@ public class MonsterAnimator : MonoBehaviour
 {
 
     private readonly int AttackTriggerHash = Animator.StringToHash("AttackTrigger");
+    private readonly int AttackEndTriggerHash = Animator.StringToHash("AttackEndTrigger");
     private readonly int SpellTriggerHash = Animator.StringToHash("SpellTrigger");
+    private readonly int SpellEndTriggerHash = Animator.StringToHash("SpellEndTrigger");
     private readonly int DieTriggerHash = Animator.StringToHash("DieTrigger");
 
     private Animator animator;
@@ -17,5 +19,11 @@ public class MonsterAnimator : MonoBehaviour
         animator = GetComponent<Animator>();
 
     }
+
+    public void SetAttackTrigger() => animator.SetTrigger(AttackTriggerHash);
+    public void SetAttackEndTrigger() => animator.SetTrigger(AttackEndTriggerHash);
+    public void SetSpellTrigger() => animator.SetTrigger(SpellTriggerHash);
+    public void SetSpellEndTrigger()=> animator.SetTrigger(SpellEndTriggerHash);
+    public void SetDieTrigger() => animator.SetTrigger(DieTriggerHash);
 
 }
